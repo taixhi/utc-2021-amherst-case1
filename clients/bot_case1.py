4152b248595dd6fb525e5ca016c726cbf9526abf
tkato24 <tkato24@amherst.edu>
2021-04-16 20:05:18 -0400

    updated

--- clients/bot_case1.py
+++ clients/bot_case1.py
@@ -18,6 +18,8 @@ DAYS_IN_YEAR = 252
 LAST_RATE_ROR_USD = 0.25
 LAST_RATE_HAP_USD = 0.5
 LAST_RATE_HAP_ROR = 2
+TODAY = 0
+YEAR = 0
 
 
 TICK_SIZES = {'6RH': 0.00001, '6RM': 0.00001, '6RU': 0.00001, '6RZ': 0.00001, '6HH': 0.00002, \
@@ -25,6 +27,7 @@ TICK_SIZES = {'6RH': 0.00001, '6RM': 0.00001, '6RU': 0.00001, '6RZ': 0.00001, '6
 LOT_SIZES = {'6RH': 100000, '6RM': 100000, '6RU': 100000, '6RZ': 100000, '6HH': 100000, \
     '6HM': 100000, '6HU': 100000, '6HZ': 100000, 'RHH': 50000, 'RHM': 50000, 'RHU': 50000, 'RHZ': 50000, "RORUSD": 100000}
 FUTURES = [i+j for i in ["6R", "6H", "RH"] for j in ["H", "M", "U", "Z"]]
+FUTURES_EXPIRY = {"H": 63, "M": 126, "U": 189, "Z": 252}
 
 
 
@@ -44,6 +47,18 @@ def IsInt(s):
     except ValueError:
         return 0
 
+''' Returns base and quote for a given asset as string '''
+def parseAssetName(asset):
+    if(asset[0] == '6'):
+        quote = 'USD'
+        if(asset[1] == 'R'):
+            base = 'ROR'
+        else:
+            base = 'HAP'
+    else:
+        base = 'HAP'
+        quote = 'ROR'
+    return base, quote
 
 class PositionTrackerBot(UTCBot):
     """
@@ -56,8 +71,10 @@ class PositionTrackerBot(UTCBot):
         Places and modifies a single bid, storing it by asset
         based upon the basic market making functionality
         """
-        orders = await self.basic_mm(asset, self.fair[asset], self.params["edge"],
-            self.params["size"], self.params["limit"],self.max_widths[asset])
+        if FUTURES_EXPIRY[asset[2]] < TODAY:
+            return
+        orders = await self.basic_mm(asset, self.fair[asset], self.edges[asset],
+            self.size[asset], self.params["limit"],self.max_widths[asset])
         for index, price in enumerate(orders['bid_prices']):
             if orders['bid_sizes'][index] != 0:
                 resp = await self.modify_order(
@@ -75,8 +92,10 @@ class PositionTrackerBot(UTCBot):
         Places and modifies a single bid, storing it by asset
         based upon the basic market making functionality
         """
-        orders = await self.basic_mm(asset, self.fair[asset], self.params["edge"],
-            self.params["size"], self.params["limit"],self.max_widths[asset])
+        if FUTURES_EXPIRY[asset[2]] < TODAY:
+            return
+        orders = await self.basic_mm(asset, self.fair[asset], self.edges[asset],
+            self.size[asset], self.params["limit"],self.max_widths[asset])
         for index, price in enumerate(orders['ask_prices']):
             if orders['ask_sizes'][index] != 0:
                 resp = await self.modify_order(
@@ -94,36 +113,47 @@ class PositionTrackerBot(UTCBot):
         """
         Modify your long term fair values based on market updates, statistical calculations, 
         etc. 
+
+        Calculate based off of
+        1) interest parity
+        2) mid price
+        3) 
         """
-        LAST_RATE_ROR_USD = 0.25
-        LAST_RATE_HAP_USD = 0.5
-        LAST_RATE_HAP_ROR = 2
+
         for asset in FUTURES:
-            if(asset[0] == '6'):
-                base = 'USD'
-                if(asset[1] == 'R'):
-                    quote = 'ROR'
-                else:
-                    quote = 'ROR'
-            else:
-                base = 'ROR'
-                quote = 'HAP'
-            spot = self.mid[asset]
-            if spot == None:
+            expiry = FUTURES_EXPIRY[asset[2]]
+            if expiry > TODAY: # Check if expired
+                self.fair[asset] = self.mid[asset]
+                spot = self.mid[asset]
+                base, quote = parseAssetName(asset)
                 if(base == 'ROR'):
-                    self.fair[asset] = LAST_RATE_ROR_USD
+                    last = LAST_RATE_ROR_USD
                 elif(quote == 'USD'):
-                    self.fair[asset] = LAST_RATE_HAP_USD
+                    last = LAST_RATE_HAP_USD
                 else:
-                    self.fair[asset] = LAST_RATE_HAP_ROR
-            else:
-                ir_base = self.interestRates[base]
-                ir_quote = self.interestRates[quote]
-                print(asset + ": Base IR=" + ir_base + ", Quote IR=" + ir_quote)
-                fair = round_nearest(float(spot) * float(ir_base) / float(ir_quote), TICK_SIZES[asset])
-                print("SPOT: " + str(spot) + ", FAIR: " + str(fair))
-                self.fair[asset] = fair
-        self.fair['RORUSD'] = self.mid['RORUSD']
+                    last = LAST_RATE_HAP_ROR
+                if spot == None:
+                    fair = last
+                else:
+                    ir_base = math.pow(self.interestRates[base],expiry-TODAY)
+                    ir_quote = math.pow(self.interestRates[quote],expiry-TODAY)
+                    # print(asset + ": Base IR=" + str(ir_base) + ", Quote IR=" + str(ir_quote))
+                    t = (DAYS_IN_YEAR-TODAY)/DAYS_IN_YEAR
+                    spot = last*t+spot*(1-t)
+                    forwardInterestParity = round_nearest(spot * float(ir_base) / float(ir_quote), TICK_SIZES[asset])
+                    fair = forwardInterestParity
+                    # fair = '''forwardInterestParity*(0.6)*(1-t) + self.mid[asset]*0.2 + '''t*0.2*last
+                    # print("SPOT: " + str(spot) + ", FAIR: " + str(fair))
+            else: # use mid price if expired
+                fair = self.mid[asset]
+            self.fair[asset] = fair # Updates the fair price across the bot
+        spot = self.mid['RORUSD']
+        if spot == None:
+            self.fair['RORUSD'] = LAST_RATE_ROR_USD
+        else: 
+            ir_base = math.pow(self.interestRates['ROR'],expiry-TODAY)
+            ir_quote = math.pow(self.interestRates['USD'],expiry-TODAY)
+            self.fair['RORUSD'] = round_nearest(float(spot) * float(ir_base) / float(ir_quote), TICK_SIZES['RORUSD'])
     
     async def spot_market(self):
         """
@@ -138,19 +168,20 @@ class PositionTrackerBot(UTCBot):
         net_position = round(net_position)
         bids_left = self.params["spot_limit"] - self.pos["RORUSD"]
         asks_left = self.params["spot_limit"] + self.pos["RORUSD"]
+
         if bids_left <= 0:
             resp = await self.place_order(
                 "RORUSD",
                 pb.OrderSpecType.MARKET,
                 pb.OrderSpecSide.ASK,
-                abs(bids_left),
+                abs(bids_left) + 1,
             )
         elif asks_left <= 0: 
             resp = await self.place_order(
                 "RORUSD",
                 pb.OrderSpecType.MARKET,
                 pb.OrderSpecSide.BID,
-                abs(asks_left),
+                abs(asks_left) + 1,
             )
         elif (net_position > 0):
             resp = await self.place_order(
@@ -176,12 +207,12 @@ class PositionTrackerBot(UTCBot):
         Max_Pos - The maximum number of contracts you are willing to hold (we just use risk limit here)
         Max_Range - The greatest you are willing to adjust your fair value by
         """
-        # print("Fair" + json.dumps(self.fair)) 
 
         ##The rate at which you fade is optimized so that you reach your max position
         ##at the same time you reach maximum range on the adjusted fair
         fade = (max_range / 2.0) / max_pos
         adjusted_fair = fair - self.pos[asset] * fade
+
 
         ##Best bid, best ask prices
         bid_p = adjusted_fair - width / 2.0
@@ -192,11 +223,12 @@ class PositionTrackerBot(UTCBot):
             bid_p - TICK_SIZES[asset])
         ask_p2 = min(adjusted_fair + clip * fade + width / 2.0, 
             ask_p + TICK_SIZES[asset])
+        # print('BID/ASK for ',asset, ": ", bid_p, ask_p)
         
         ##Remaining ability to quote
         bids_left = max_pos - self.pos[asset]
         asks_left = max_pos + self.pos[asset]
-
+        # print("For asset " + asset + ", you have " + str(bids_left) + "bids left, and " + str(asks_left) + "asks left.")
         if bids_left <= 0:
             #reduce your position as you are violating risk limits!
             ask_p = bid_p
@@ -239,21 +271,25 @@ class PositionTrackerBot(UTCBot):
         self.pos = {asset:0 for asset in FUTURES + ["RORUSD"]}
         self.fair = {asset:5 for asset in FUTURES + ["RORUSD"]}
         self.mid = {asset: None for asset in FUTURES + ["RORUSD"]}
-        self.max_widths = {asset:0.005 for asset in FUTURES}
+        self.max_widths = {asset:0.1 for asset in FUTURES}
         
         self.bidorderid = {asset:["",""] for asset in FUTURES}
         self.askorderid = {asset:["",""] for asset in FUTURES}
 
-        self.interestRates = {asset:1.01 for asset in ['ROR', 'HAP', 'USD']}
+        self.interestRates = {asset:1 for asset in ['ROR', 'HAP', 'USD']}
+        
+        self.edges = {asset:TICK_SIZES[asset]*12 for asset in FUTURES}
+        self.edges["RORUSD"] = TICK_SIZES["RORUSD"]*2
+
+        self.size = {asset:2 for asset in FUTURES}
+        self.size["RORUSD"] = 1
         """
         Constant params with respect to assets. Modify this is you would like to change
         parameters based on asset
         """
         self.params = {
-            "edge": 0.05,
-            "limit": 100,
-            "size": 10,
-            "spot_limit": 10
+            "limit": 90,
+            "spot_limit": 5
         }
     async def handle_exchange_update(self, update: pb.FeedMessage):
         kind, _ = betterproto.which_one_of(update, "msg")
@@ -270,7 +306,7 @@ class PositionTrackerBot(UTCBot):
             for asset in (["RH" + j for j in ["H", "M", "U", "Z"]]):
                 my_m2m += (self.mid[asset] * self.pos[asset] * self.mid["RORUSD"] 
                     if (self.mid[asset] is not None and self.mid["RORUSD"] is not None) else 0)
-            print("M2M", update.pnl_msg.m2m_pnl, my_m2m)
+            print("M2M", update.pnl_msg.realized_pnl, update.pnl_msg.m2m_pnl, my_m2m)
         #Update position upon fill messages of your trades
         elif kind == "fill_msg":
             if update.fill_msg.order_side == pb.FillMessageSide.BUY:
@@ -304,22 +340,28 @@ class PositionTrackerBot(UTCBot):
         elif kind == "generic_msg":
             data = update.generic_msg.message.split(',')
             if(0 < IsInt(data[0])):
-                self.interestRates['ROR'] = data[1]
-                self.interestRates['HAP'] = data[2]
-                self.interestRates['USD'] = data[3]
+                TODAY = data[0]
+                self.interestRates['ROR'] = daily_rate(float(data[1]))
+                self.interestRates['HAP'] = daily_rate(float(data[2]))
+                self.interestRates['USD'] = daily_rate(float(data[3]))
+                print(update.generic_msg.message)
+                # print(self.interestRates['ROR'], self.interestRates['HAP'], self.interestRates['USD'])
                 await self.evaluate_fairs()
+                print(self.fair)
+                print(self.mid)
+                diffs = {asset: (self.fair[asset] - self.mid[asset] if self.mid[asset] != None else 0) for asset in FUTURES}
+                # print(diffs)
+                for asset in FUTURES:
+                    await self.place_bids(asset)
+                    await self.place_asks(asset)
+                await self.spot_market()
             elif("New Federal Funds Target" in data[0]):
                 d = data.split(" ")
                 currency = d[0]
                 target = d[0]
                 print("New Federal Funds Target for " + currency + ":" + target)
             else:
+                pass
                 print(update.generic_msg.message)
-
-            for asset in FUTURES:
-                await self.place_bids(asset)
-                await self.place_asks(asset)
-            await self.spot_market()
-
 if __name__ == "__main__":
     start_bot(PositionTrackerBot)
